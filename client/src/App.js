669ff34e06d0d0ec710baeb5f7d7b9cba05976c8
arshaqu <muhammedarshaque@gmail.com>
2024-01-31 15:43:53 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div className="App p-5">
+      <h1>Book Shop</h1>
+      <button style={{marginLeft:"20px"}} className="btn btn-primary" type="submit">Button</button>
+    </div>
+  );
+}
+
+export default App;
